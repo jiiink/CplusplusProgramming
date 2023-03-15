b5f5b75301d65b05e41dbb8617f60862e6b96e8d
ByeongHyeon Kim <bhkim0404@gmail.com>
2023-03-15 20:59:07 +0900

    writing function...

--- Assignment/chap2AssignMatrix/exercise_OneArray_1.cpp
+++ Assignment/chap2AssignMatrix/exercise_OneArray_1.cpp
@@ -11,8 +11,10 @@
 #define CCOLS 5
 //int getMatrix(int[][COLS], int);//난수 생성하여 처리 - pointer만 사용하여 구현
 bool showMatrix(int[][COLS], int);
+bool showMatrixB(int(*arr)[BCOLS], int rows);
 bool addMatrix(int[][COLS], int, int[][COLS], int, int[][COLS], int);
-bool getMatrix(int(*arr)[COLS], int rows);
+bool getMatrixA(int(*arr)[COLS], int rows);
+bool getMatrixB(int(*arr)[BCOLS], int rows);
 bool multiplyMatrix(int[][COLS], int, int[][BCOLS], int, int[][CCOLS], int);
 void main()
 {
@@ -21,21 +23,26 @@ void main()
     int b[BROWS][BCOLS];
     int c[CROWS][CCOLS];
 
-    getMatrix(a, ROWS);
+    getMatrixA(a, ROWS);
+    printf("a\n");
     showMatrix(a, ROWS);
-    getMatrix(a1, ROWS);
+    getMatrixA(a1, ROWS);
+    printf("a1\n");
     showMatrix(a1, ROWS);
    
     addMatrix(a, ROWS, a1, ROWS, a2, ROWS);
+    printf("a2\n");
     showMatrix(a2, CROWS);
-    getMatrix(b, BROWS);
-    showMatrix(b, BROWS);
+    getMatrixB(b, BROWS);
+    printf("b\n");
+    showMatrixB(b, BROWS);
     multiplyMatrix(a, ROWS, b, BROWS, c, CROWS);
     showMatrix(c, CROWS);
+    
     system("pause");
 }
 //int getMatrix(int(*arr)[], int rows, int cols)
-bool getMatrix(int(*arr)[COLS], int rows)
+bool getMatrixA(int(*arr)[COLS], int rows)
 {
     for (int i = 0; i < rows; i++)
     {
@@ -45,4 +52,62 @@ bool getMatrix(int(*arr)[COLS], int rows)
         }
     }
     return true;
+}
+
+bool getMatrixB(int(*arr)[BCOLS], int rows)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            *(*(arr + i) + j) = rand() % 10;
+        }
+    }
+    return true;
+}
+
+bool showMatrix(int(*arr)[COLS], int rows) {
+    printf("\n");
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d ", *(*(arr + i) + j));
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    return true;
+}
+
+bool showMatrixB(int(*arr)[BCOLS], int rows) {
+    printf("\n");
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d ", *(*(arr + i) + j));
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    return true;
+}
+
+bool addMatrix(int(*arr1)[COLS], int row1, int(*arr2)[COLS], int row2, int(*arr3)[COLS], int row3) {
+    for (int i = 0; i < row1; i++) {
+        for (int j = 0; j < COLS; j++) {
+            *(*(arr3 + i) + j) = *(*(arr1 + i) + j) + *(*(arr2 + i) + j);
+        }
+    }
+    return true;
+}
+
+bool multiplyMatrix(int(*arr1)[COLS], int row1, int(*arr2)[BCOLS], int row2, int(*arr3)[CCOLS], int row3) {
+    int value;
+    for (int i = 0; i < row1; i++) {
+        for (int j = 0; j < COLS; j++) {
+            value += *(*(arr1 + i) + j) + *(*(arr2 + j) + i);
+        }
+    }
 }
